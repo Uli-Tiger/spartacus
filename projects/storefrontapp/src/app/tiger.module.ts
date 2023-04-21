0efe7bbbf185533b93211af2c1587bb014fc2d0e
Christoph Hinssen <christoph.hinssen@sap.com>
2023-04-21 08:08:14 +0200

    test: fix variant carousel test

---
+++ projects/storefrontapp/src/app/tiger.module.ts
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2023 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { NgModule } from '@angular/core';
+import { provideConfig } from '@spartacus/core';
+import {
+  ConfiguratorCoreConfig,
+  ConfiguratorUISettingsConfig,
+} from '@spartacus/product-configurator/rulebased';
+@NgModule({
+  providers: [
+    provideConfig(<ConfiguratorUISettingsConfig>{
+      productConfigurator: {
+        enableNavigationToConflict: true,
+      },
+    }),
+    provideConfig(<ConfiguratorCoreConfig>{
+      productConfigurator: {
+        enableVariantSearch: false,
+      },
+    }),
+  ],
+})
+export class TigerModule {}
